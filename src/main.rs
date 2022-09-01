2fc4790292c479edb1da4aeedcd3550ffc84b0bb
Jesse Leung <jessetleung@aol.com>
2022-09-01 18:31:11 +0900

    [Add] Add ability to check outlet page

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,15 @@ use line::messages::{SendMessageType, TextMessage};
 use dotenv::dotenv;
 
 const PAGE_URL: &str = "https://www.canyon.com/en-jp/road-bikes/race-bikes/ultimate/cf-sl/ultimate-cf-sl-7-wmn-disc/3068.html?dwvar_3068_pv_rahmenfarbe=BU%2FTQ";
+const OUTLET_PAGE_URL: &str = "https://www.canyon.com/en-jp/outlet-bikes/?hideSelectedFilters=true&prefn1=pc_outlet&prefn2=pc_rahmengroesse&prefv1=true&prefv2=3XS%7C2XS%7CXS";
 const LI_SELECTOR: &str = "li.productConfiguration__optionListItem";
 const BUTTON_SELECTOR: &str = "button.productConfiguration__selectVariant";
+const PRODUCT_TILE_SELECTOR: &str = "div.js-productTileWrapper";
+const PRODUCT_NAME_SELECTOR: &str = "div.productTile__productName";
+const PRODUCT_SIZE_SELECTOR: &str = "div.eyebrow";
 const ATTR: &str = "data-product-size";
 const TARGET_SIZE: &str = "2XS";
+const TARGET_NAME: &str = "Ultimate CF SL 7 WMN Disc";
 const UPDATE_INTERVAL_SECONDS: u64 = 600;
 
 fn main() {
@@ -19,7 +24,10 @@ fn main() {
         println!("Checking for stock");
         let interval = time::Duration::from_secs(UPDATE_INTERVAL_SECONDS);
         if check_stock() {
-            send_message();
+            send_message(false);
+        }
+        if check_outlet_stock() {
+            send_message(true);
         }
         thread::sleep(interval);
     }
@@ -27,9 +35,9 @@ fn main() {
 
 fn check_stock() -> bool {
     let response = reqwest::blocking::get(PAGE_URL)
-    .unwrap()
-    .text()
-    .unwrap();
+        .unwrap()
+        .text()
+        .unwrap();
 
     let document = scraper::Html::parse_document(&response);
 
@@ -53,13 +61,44 @@ fn check_stock() -> bool {
     return false;
 }
 
-fn send_message() {
+fn check_outlet_stock() -> bool {
+    let response = reqwest::blocking::get(OUTLET_PAGE_URL)
+        .unwrap()
+        .text()
+        .unwrap();
+
+    let document = scraper::Html::parse_document(&response);
+
+    let product_tile_selector = scraper::Selector::parse(PRODUCT_TILE_SELECTOR).unwrap();
+    let product_name_selector = scraper::Selector::parse(PRODUCT_NAME_SELECTOR).unwrap();
+    let product_size_selector = scraper::Selector::parse(PRODUCT_SIZE_SELECTOR).unwrap();
+
+    for element in document.select(&product_tile_selector) {
+        let name_element = element.select(&product_name_selector).next();
+        let size_element = element.select(&product_size_selector).next();
+
+        let is_name = match name_element {
+            Some(name) => name.inner_html().contains(TARGET_NAME),
+            None => continue
+        };
+        let is_size = match size_element {
+            Some(size) => size.inner_html().contains(TARGET_SIZE),
+            None => continue
+        };
+        if is_name && is_size {
+            return true;
+        }
+    }
+    return false;
+}
+
+fn send_message(isOutlet: bool) {
     let channel_secret: &str = &env::var("CHANNEL_SECRET").expect("Failed to get channel secret.");
     let channel_token: &str = &env::var("CHANNEL_TOKEN").expect("Failed to get channel token");
     
     let bot = LineBot::new(channel_secret, channel_token);
 
-    let message_text = format!("There's a 2XS ultimate in stock!\n{}", PAGE_URL);
+    let message_text = format!("There's a 2XS ultimate in stock!\n{}", if isOutlet { PAGE_URL } else { OUTLET_PAGE_URL} );
 
     let message = SendMessageType::TextMessage(TextMessage {
         text: message_text,
